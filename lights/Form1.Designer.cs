451a93406407c7dd1df306e27cc67c9315612e29
RamyaMahendran <ramyamahendran19@gmail.com>
2015-11-05 20:45:01 -0800

    initializing

---
+++ lights/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace lights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnw1 = new System.Windows.Forms.Button();
+            this.btnw2 = new System.Windows.Forms.Button();
+            this.btnw3 = new System.Windows.Forms.Button();
+            this.btnn1 = new System.Windows.Forms.Button();
+            this.btnn2 = new System.Windows.Forms.Button();
+            this.btnn3 = new System.Windows.Forms.Button();
+            this.btne1 = new System.Windows.Forms.Button();
+            this.btne2 = new System.Windows.Forms.Button();
+            this.btne3 = new System.Windows.Forms.Button();
+            this.btns1 = new System.Windows.Forms.Button();
+            this.btns2 = new System.Windows.Forms.Button();
+            this.btns3 = new System.Windows.Forms.Button();
+            this.btnstage = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnw1
+            // 
+            this.btnw1.Location = new System.Drawing.Point(270, 221);
+            this.btnw1.Name = "btnw1";
+            this.btnw1.Size = new System.Drawing.Size(34, 23);
+            this.btnw1.TabIndex = 0;
+            this.btnw1.Text = "W1";
+            this.btnw1.UseVisualStyleBackColor = true;
+            // 
+            // btnw2
+            // 
+            this.btnw2.Location = new System.Drawing.Point(270, 192);
+            this.btnw2.Name = "btnw2";
+            this.btnw2.Size = new System.Drawing.Size(34, 23);
+            this.btnw2.TabIndex = 1;
+            this.btnw2.Text = "W2";
+            this.btnw2.UseVisualStyleBackColor = true;
+            this.btnw2.Click += new System.EventHandler(this.btnw2_Click);
+            // 
+            // btnw3
+            // 
+            this.btnw3.Location = new System.Drawing.Point(270, 163);
+            this.btnw3.Name = "btnw3";
+            this.btnw3.Size = new System.Drawing.Size(34, 23);
+            this.btnw3.TabIndex = 2;
+            this.btnw3.Text = "W3";
+            this.btnw3.UseVisualStyleBackColor = true;
+            this.btnw3.Click += new System.EventHandler(this.btnw3_Click);
+            // 
+            // btnn1
+            // 
+            this.btnn1.Location = new System.Drawing.Point(591, 154);
+            this.btnn1.Name = "btnn1";
+            this.btnn1.Size = new System.Drawing.Size(30, 23);
+            this.btnn1.TabIndex = 3;
+            this.btnn1.Text = "N1";
+            this.btnn1.UseVisualStyleBackColor = true;
+            this.btnn1.Click += new System.EventHandler(this.btnn1_Click);
+            // 
+            // btnn2
+            // 
+            this.btnn2.Location = new System.Drawing.Point(672, 154);
+            this.btnn2.Name = "btnn2";
+            this.btnn2.Size = new System.Drawing.Size(31, 23);
+            this.btnn2.TabIndex = 4;
+            this.btnn2.Text = "N2";
+            this.btnn2.UseVisualStyleBackColor = true;
+            // 
+            // btnn3
+            // 
+            this.btnn3.Location = new System.Drawing.Point(753, 154);
+            this.btnn3.Name = "btnn3";
+            this.btnn3.Size = new System.Drawing.Size(36, 23);
+            this.btnn3.TabIndex = 5;
+            this.btnn3.Text = "N3";
+            this.btnn3.UseVisualStyleBackColor = true;
+            // 
+            // btne1
+            // 
+            this.btne1.Location = new System.Drawing.Point(782, 269);
+            this.btne1.Name = "btne1";
+            this.btne1.Size = new System.Drawing.Size(33, 23);
+            this.btne1.TabIndex = 6;
+            this.btne1.Text = "E1";
+            this.btne1.UseVisualStyleBackColor = true;
+            // 
+            // btne2
+            // 
+            this.btne2.Location = new System.Drawing.Point(782, 298);
+            this.btne2.Name = "btne2";
+            this.btne2.Size = new System.Drawing.Size(33, 23);
+            this.btne2.TabIndex = 7;
+            this.btne2.Text = "E2";
+            this.btne2.UseVisualStyleBackColor = true;
+            // 
+            // btne3
+            // 
+            this.btne3.Location = new System.Drawing.Point(782, 327);
+            this.btne3.Name = "btne3";
+            this.btne3.Size = new System.Drawing.Size(33, 23);
+            this.btne3.TabIndex = 8;
+            this.btne3.Text = "E3";
+            this.btne3.UseVisualStyleBackColor = true;
+            // 
+            // btns1
+            // 
+            this.btns1.Location = new System.Drawing.Point(455, 336);
+            this.btns1.Name = "btns1";
+            this.btns1.Size = new System.Drawing.Size(36, 23);
+            this.btns1.TabIndex = 9;
+            this.btns1.Text = "S1";
+            this.btns1.UseVisualStyleBackColor = true;
+            // 
+            // btns2
+            // 
+            this.btns2.Location = new System.Drawing.Point(373, 336);
+            this.btns2.Name = "btns2";
+            this.btns2.Size = new System.Drawing.Size(31, 23);
+            this.btns2.TabIndex = 10;
+            this.btns2.Text = "S2";
+            this.btns2.UseVisualStyleBackColor = true;
+            // 
+            // btns3
+            // 
+            this.btns3.Location = new System.Drawing.Point(289, 336);
+            this.btns3.Name = "btns3";
+            this.btns3.Size = new System.Drawing.Size(30, 23);
+            this.btns3.TabIndex = 11;
+            this.btns3.Text = "S3";
+            this.btns3.UseVisualStyleBackColor = true;
+            // 
+            // btnstage
+            // 
+            this.btnstage.Location = new System.Drawing.Point(502, 252);
+            this.btnstage.Name = "btnstage";
+            this.btnstage.Size = new System.Drawing.Size(87, 23);
+            this.btnstage.TabIndex = 12;
+            this.btnstage.Text = "Stage";
+            this.btnstage.UseVisualStyleBackColor = true;
+            this.btnstage.TextChanged += new System.EventHandler(this.btnstage_TextChanged);
+            this.btnstage.Click += new System.EventHandler(this.btnstage_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnstage);
+            this.panel1.Controls.Add(this.btns3);
+            this.panel1.Controls.Add(this.btns2);
+            this.panel1.Controls.Add(this.btns1);
+            this.panel1.Controls.Add(this.btne3);
+            this.panel1.Controls.Add(this.btne2);
+            this.panel1.Controls.Add(this.btne1);
+            this.panel1.Controls.Add(this.btnn3);
+            this.panel1.Controls.Add(this.btnn2);
+            this.panel1.Controls.Add(this.btnn1);
+            this.panel1.Controls.Add(this.btnw3);
+            this.panel1.Controls.Add(this.btnw2);
+            this.panel1.Controls.Add(this.btnw1);
+            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1093, 512);
+            this.panel1.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1149, 481);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnw1;
+        private System.Windows.Forms.Button btnw2;
+        private System.Windows.Forms.Button btnw3;
+        private System.Windows.Forms.Button btnn1;
+        private System.Windows.Forms.Button btnn2;
+        private System.Windows.Forms.Button btnn3;
+        private System.Windows.Forms.Button btne1;
+        private System.Windows.Forms.Button btne2;
+        private System.Windows.Forms.Button btne3;
+        private System.Windows.Forms.Button btns1;
+        private System.Windows.Forms.Button btns2;
+        private System.Windows.Forms.Button btns3;
+        private System.Windows.Forms.Button btnstage;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ lights/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lights
+{
+    public partial class Form1 : Form
+    {
+        int turn=1;
+        int p;
+        Button[] btns;
+        int []Stage1;
+        int []Stage2;
+        int []Stage3;
+        int []Stage4;
+        int []Stage5;
+        int []Stage6;
+        public Form1()
+        {
+            InitializeComponent();
+            btns = new Button[] { btnn1, btnn2, btnn3, btne1, btne2, btne3, btns1, btns2, btns3, btnw1, btnw2, btnw3 };
+            Stage1 = new int[] { 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1 };
+            Stage2 = new int[] { 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1 };
+            Stage3 = new int[] { 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1 };
+            Stage4 = new int[] { 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1 };
+            Stage5 = new int[] { 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1 };
+            Stage6 = new int[] { 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1 };
+           // turnLights();
+        }
+
+        void LightGlow(int[] arr)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (arr[i] == 1)
+                    btns[i].BackColor = Color.Green;
+                else
+                    btns[i].BackColor = Color.Red;
+            }
+        }
+
+        private void btnw3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnw2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btnstage_Click(object sender, EventArgs e)
+        {
+            //turnLights();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            turn = turn + 1;
+            p = (turn % 6) + 1;
+            btnstage.Text = p.ToString();
+        }
+
+        private void btnstage_TextChanged(object sender, EventArgs e)
+        {
+            switch (p)
+            {
+                case 1: LightGlow(Stage1); break;
+                case 2: LightGlow(Stage2); break;
+                case 3: LightGlow(Stage3); break;
+                case 4: LightGlow(Stage4); break;
+                case 5: LightGlow(Stage5); break;
+                case 6: LightGlow(Stage6); break;
+                default: break;
+            }
+        }
+    }
+}
